186d481e79963b324304404caa70a8084e82192e
Altaf Quadri <altafmquadri@gmail.com>
2020-02-23 23:06:08 -0500

    1 TwoSum

---
+++ 1 TwoSum.js
@@ -0,0 +1,24 @@
+/*  Two Sum
+Given an array of integers, return indices of 
+the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]. */
+
+const twoSum = (nums, target) => {
+    let numsHash = {}
+    for (let i = 0; i < nums.length; i++) {
+        let value = nums[i]
+        if (numsHash[target - value] !== undefined) return [numsHash[target - value], i]
+        numsHash[value] = i
+    }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
