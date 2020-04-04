cb12c412a24b979d76773a409960e56e1078c89f
Altaf Quadri <altafmquadri@gmail.com>
2020-04-04 16:24:55 -0400

    valid parenthesis

---
+++ 20 ValidParenthesis.js
@@ -0,0 +1,58 @@
+/* Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true */
+
+const isValid = s => {
+    if (!s.length) return true
+    if (s.length % 2 !== 0) return false
+    s = s.split('')
+    let stack = []
+    let valid = false
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') stack.push(s[i])
+
+        if (s[i] === ')' && stack.slice(-1)[0] === '(') stack.pop(), valid = true
+        if (s[i] === ']' && stack.slice(-1)[0] === '[') stack.pop(), valid = true
+        if (s[i] === '}' && stack.slice(-1)[0] === '{') stack.pop(), valid = true
+    }
+    if (stack.length) return false
+    return valid
+}
+
+
+console.log(isValid("()")) //true
+console.log(isValid("()[]{}")) //true
+console.log(isValid("(]"))//false
+console.log(isValid("([)]"))//false
+console.log(isValid("{[]}"))//true
+console.log(isValid(""))//true
+console.log(isValid("["))// false
+console.log(isValid("(("))//false
+console.log(isValid("(()("))//false
+console.log(isValid(("{{)}")))//false
