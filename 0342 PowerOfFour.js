9ab5d68e1a1e6643a749fc87cfd07772231bd9ef
Altaf Quadri <altafmquadri@gmail.com>
2020-04-23 20:53:21 -0400

    power of four

---
+++ 0342 PowerOfFour.js
@@ -0,0 +1,20 @@
+/* Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion? */
+
+const isPowerOfFour = num => {
+    if (num < 1) return false
+    while (num % 4 === 0) num /= 4
+    return num === 1
+}
+
+console.log(isPowerOfFour(16))
+console.log(isPowerOfFour(5))
