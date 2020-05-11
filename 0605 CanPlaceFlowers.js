bc137c370bd96bf3921a8655c240c347ef84ee2f
Altaf Quadri <altafmquadri@gmail.com>
2020-05-10 21:36:07 -0400

    can place flowers

---
+++ 0605 CanPlaceFlowers.js
@@ -0,0 +1,33 @@
+/* Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+
+Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: True
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: False
+Note:
+The input array won't violate no-adjacent-flowers rule.
+The input array size is in the range of [1, 20000].
+n is a non-negative integer which won't exceed the input array size. */
+
+const canPlaceFlowers = (flowerbed, n) => {
+    if (n === 0) return true
+    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+        if (flowerbed[i] === 0 &&
+            flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+            flowerbed[i] = 1
+            n--
+        }
+    }
+    return n === 0
+}
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1))//true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2))//false
+console.log(canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 0)) //true
+console.log(canPlaceFlowers([1, 0, 0, 0, 1, 0, 1], 1)) //true
+console.log(canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 1)) //false
+console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1)) //true
